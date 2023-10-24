d1588ce6e133398ebe9622700bcc07cbd0e9c004
shriyanm <146664902+shriyanm@users.noreply.github.com>
2023-10-25 00:05:36 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "c. Hyper Text Markup Language",
+        options: [
+            "a. Hyper Text Multi Language",
+            "b. Hyper Text Multiple Language",
+            "c. Hyper Text Markup Language",
+            "d. Home Text Multi Language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "a. Cascading Style Sheet",
+        options: [
+            "a. Cascading Style Sheet",
+            "b. Cute Style Sheet",
+            "c. Computer Style Sheet",
+            "d. Cooperative Style Sheet"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "a. Hypertext Preprocessor",
+        options: [
+            "a. Hypertext Preprocessor",
+            "b. Hometext Programming",
+            "c. Hypertext Preprogramming",
+            "d. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "d. Structured Query Language",
+        options: [
+            "a. Strength Query Language",
+            "b. Stylesheet Query Language",
+            "c. Science Query Language",
+            "d. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Does XML stand for Extensible Markup Language?",
+        answer: "a. True",
+        options: [
+            "a. True",
+            "b. False",
+            "c. Maybe True",
+            "d. Maybe False"
+        ]
+    }
+];
